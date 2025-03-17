0b22ef656503389f9af776ba0c047024bce58dec
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2025-03-17 15:37:42 +0100

    Working detail page access

--- src/components/camel-integration-details/CamelIntegrationDetails.tsx
+++ src/components/camel-integration-details/CamelIntegrationDetails.tsx
@@ -4,23 +4,17 @@ import { Card, CardBody, CardTitle, TextContent } from '@patternfly/react-core';
 import { useTranslation } from 'react-i18next';
 import { ResourceLink } from '@openshift-console/dynamic-plugin-sdk';
 import { camelIntegrationGVK, getCamelIntegrationVersion, getBuildTimestamp } from '../../utils';
+import { METADATA_ANNOTATION_CAMEL_CEQ_VERSION, METADATA_ANNOTATION_CAMEL_CSB_VERSION, METADATA_ANNOTATION_CAMEL_QUARKUS_PLATFORM_VERSION, METADATA_ANNOTATION_CAMEL_SPRINGBOOT_VERSION, METADATA_ANNOTATION_CAMEL_VERSION } from '../../const';
 
 type CamelIntegrationDetailsProps = {
     obj: CamelIntegrationKind;
 };
 
 const CamelIntegrationDetails: React.FC<CamelIntegrationDetailsProps> = ({ obj: camelInt }) => {
-    console.log("+++CamelIntegrationDetails");
     const { t } = useTranslation('plugin__camel-openshift-console-plugin');
 
-
-    console.log("is camelInt there?");
-    console.log(typeof camelInt);
-    console.log("detail");
-    console.log(camelInt);
-
     const groupVersionKind = camelIntegrationGVK(camelInt.kind);
-    const version = getCamelIntegrationVersion(camelInt);
+    const camelVersion = getCamelIntegrationVersion(camelInt);
     const buildTimestamp = getBuildTimestamp(camelInt);
 
 
@@ -38,16 +32,25 @@ const CamelIntegrationDetails: React.FC<CamelIntegrationDetailsProps> = ({ obj: 
                             namespace={camelInt.metadata.namespace}
                             linkTo={true}
                         />
-                        {version && (
-                            <TextContent>
-                                <strong>Version:</strong> {version}
-                            </TextContent>
-                        )}
-                        {buildTimestamp && (
-                            <TextContent>
-                                <strong>Build Timestamp:</strong> {buildTimestamp}
-                            </TextContent>
-                        )}
+                        <TextContent>
+                            <strong>Version: </strong>
+                            {camelVersion || <span className="text-muted">{t('No camel version')}</span>}
+                        </TextContent>
+                        <TextContent>
+                            <strong>Build Timestamp: </strong>
+                            {buildTimestamp || <span className="text-muted">{t('No build timestamp')}</span>}
+                        </TextContent>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardTitle>Frameworks</CardTitle>
+                    <CardBody>
+                        {camelInt.metadata.annotations?.[METADATA_ANNOTATION_CAMEL_VERSION] && <TextContent><strong>Camel: </strong> {camelInt.metadata.annotations[METADATA_ANNOTATION_CAMEL_VERSION]}</TextContent>}
+                        {camelInt.metadata.annotations?.[METADATA_ANNOTATION_CAMEL_QUARKUS_PLATFORM_VERSION] && <TextContent><strong>Quarkus Platform: </strong> {camelInt.metadata.annotations[METADATA_ANNOTATION_CAMEL_QUARKUS_PLATFORM_VERSION]}</TextContent>}
+                        {camelInt.metadata.annotations?.[METADATA_ANNOTATION_CAMEL_CEQ_VERSION] && <TextContent><strong>Camel Quarkus: </strong> {camelInt.metadata.annotations[METADATA_ANNOTATION_CAMEL_CEQ_VERSION]}</TextContent>}
+
+                        {camelInt.metadata.annotations?.[METADATA_ANNOTATION_CAMEL_CSB_VERSION] && <TextContent><strong>Camel Spring Boot: </strong> {camelInt.metadata.annotations[METADATA_ANNOTATION_CAMEL_CSB_VERSION]}</TextContent>}
+                        {camelInt.metadata.annotations?.[METADATA_ANNOTATION_CAMEL_SPRINGBOOT_VERSION] && <TextContent><strong>Spring Boot: </strong> {camelInt.metadata.annotations[METADATA_ANNOTATION_CAMEL_SPRINGBOOT_VERSION]}</TextContent>}
                     </CardBody>
                 </Card>
 
