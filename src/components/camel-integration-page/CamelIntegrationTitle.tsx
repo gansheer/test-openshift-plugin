302054c2ef6625e95f442f9a8a76b2cd6d803a19
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2025-03-24 10:46:25 +0100

    Lint

--- src/components/camel-integration-page/CamelIntegrationTitle.tsx
+++ src/components/camel-integration-page/CamelIntegrationTitle.tsx
@@ -5,54 +5,46 @@ import { useTranslation } from 'react-i18next';
 import { ALL_NAMESPACES_KEY } from '../../const';
 import { useActiveNamespace } from '@openshift-console/dynamic-plugin-sdk';
 
-
 type CamelIntegrationTitleProps = {
-    name: string;
-    namespace: string;
+  name: string;
+  namespace: string;
 };
 
 export const getUrlList = (namespace): string => {
-    if (namespace === ALL_NAMESPACES_KEY) {
-        return `/camel/all-namespaces`;
-    } else {
-        return `/camel/ns/` + namespace
-    }
-}
+  if (namespace === ALL_NAMESPACES_KEY) {
+    return `/camel/all-namespaces`;
+  } else {
+    return `/camel/ns/` + namespace;
+  }
+};
 
 const CamelIntegrationTitle: React.FC<CamelIntegrationTitleProps> = ({ name }) => {
-    const { t } = useTranslation('plugin__camel-openshift-console-plugin');
+  const { t } = useTranslation('plugin__camel-openshift-console-plugin');
 
-    
-    const [activeNamespace, _] = useActiveNamespace();
-    
-    return (
-        <>
-            <div className="co-m-nav-title co-m-nav-title--detail">
-                <div>
-                    <Breadcrumb className="pf-v6-c-breadcrumb co-breadcrumb">
-                        <BreadcrumbItem>
-                            <Link to={getUrlList(activeNamespace)}>
-                                {t('Camel')}
-                            </Link>
-                        </BreadcrumbItem>
-                        <BreadcrumbItem>{t('Camel Details')}</BreadcrumbItem>
-                    </Breadcrumb>
-                </div>
-                <span className="co-m-pane__heading">
-                    <h1 className="co-resource-item__resource-name">
-                        <span
-                            className="co-m-resource-icon co-m-resource-secret co-m-resource-icon--lg"
-                            title="Camel"
-                        >
-                            {t('C')}
-                        </span>
-                        {name}
-                    </h1>
-                </span>
-            </div>
+  const [activeNamespace] = useActiveNamespace();
 
-        </>
-    );
+  return (
+    <>
+      <div className="co-m-nav-title co-m-nav-title--detail">
+        <div>
+          <Breadcrumb className="pf-v6-c-breadcrumb co-breadcrumb">
+            <BreadcrumbItem>
+              <Link to={getUrlList(activeNamespace)}>{t('Camel')}</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem>{t('Camel Details')}</BreadcrumbItem>
+          </Breadcrumb>
+        </div>
+        <span className="co-m-pane__heading">
+          <h1 className="co-resource-item__resource-name">
+            <span className="co-m-resource-icon co-m-resource-secret co-m-resource-icon--lg">
+              {t('C')}
+            </span>
+            {name}
+          </h1>
+        </span>
+      </div>
+    </>
+  );
 };
 
 export default CamelIntegrationTitle;
